a1f15b40346971bbe5782060eac37ab1fe693172
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2022-04-02 23:15:29 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const one = document.getElementById('one')
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+const four = document.getElementById("four")
+const five = document.getElementById("five")
+
+one.addEventListener('click', vadskahända?)
